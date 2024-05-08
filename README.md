d162353ba466cf62cbbd027d2e4823d382b00ef7
Juliana Zacharias <julianazacharias.dev@gmail.com>
2024-05-08 20:47:53 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# DNS Resolver
+
+This is a Python-based domain resolver tool that helps resolve domain names to their corresponding IP addresses. It can be used to quickly retrieve the IP addresses associated with various domains.
+
+## Features
+
+- Enumerate real world domains.
+- Identify real world subdomains.
+- Simple and easy-to-use interface.
+
+### Prerequisites
+
+- Python 3.x installed on your system.
+
+### Run
+
+```poetry shell```
+
+```poetry install```
+
+This will run the install script to add necessary dependencies to your system.
+
+## Example
+
+```bash
+
+$ python dns_enum.py < domain >
+$ python subdomain_enum.py < subdomain >
+
+```
---
+++ app/dns_enum.py
@@ -0,0 +1,36 @@
+import dns.resolver
+import sys
+
+record_types = {
+    'A': 'Host address',
+    'AAAA': 'IPv6 host address',
+    'NS': 'Name Server',
+    'CNAME': 'Canonical name for an alias',
+    'MX': 'Mail eXchange',
+    'PTR': 'Pointer',
+    'SOA': 'Start Of Authority',
+    'SRV' : 'Location of service',
+    'TXT': 'Descriptive text'
+}
+
+try:    
+    domain = input('Type the domain: ')
+    #domain = sys.argv[1]
+except IndexError:
+    print('Syntax error - python3 dnsenum.py domainname')
+
+for records, name in record_types.items():
+    try:
+        answer = dns.resolver.resolve(domain, records)
+        print(f'\n Record type = {records} : {name}')
+        print('-' * 70)
+        for server in answer:
+            print(server.to_text())
+    except dns.resolver.NoAnswer:
+        pass
+    except dns.resolver.NXDOMAIN:
+        print(f'{domain} does not exist.')
+        quit()
+    except KeyboardInterrupt:
+        print('Quitting.')
+        quit()
